ed479b9bca80133d1d24f4cf7bf3fc44af3551d7
Daath <540871129@qq.com>
2016-07-30 23:50:11 +0800

    fix

--- appUser/urls.py
+++ appUser/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^applyGuide/', views.apply_guide),
     url(r'^getGuideLists/', views.get_guide_lists),
     url(r'^getVisitorLists/', views.get_visitor_lists),
-    # url(r'^getVisitorInfo/', views.get_visitor_info), 没用到
+    url(r'^getVisitorInfo/', views.get_visitor_info),   # 没用到
     url(r'^avatarUpload/', views.avatar_upload),
     url(r'^tokenReload/', views.token_reload),
     url(r'^saveChannelId/', views.save_channel_id),
